aa482800f98133a46ef75ca48d1264aa129e8f39
O-Show <oshow999@gmail.com>
2015-11-15 23:13:38 +0900

    Add go-flags package to parse command line args
    I defined simple input and output file name options.

--- nhelpviz.go
+++ nhelpviz.go
@@ -1,8 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"github.com/jessevdk/go-flags"
 	"os"
 )
+
+type options struct {
+	InputFile  string `short:"i" long:"input" required:"true" description:"input .PROC file name"`
+	OutputFile string `short:"o" long:"output" default:"STDOUT" description:"output dot file name"`
+}
+
+func getOptions() (*options, error) {
+	options := new(options)
+	_, err := flags.Parse(options)
+	if err != nil {
+		return nil, err
+	}
+	return options, err
+}
 
 func main() {
 	res := process()
@@ -10,5 +26,11 @@ func main() {
 }
 
 func process() int {
+	options, err := getOptions()
+	if err != nil {
+		return 1
+	}
+
+	fmt.Println(options)
 	return 0
 }
