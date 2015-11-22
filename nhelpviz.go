623a7c6b4487553111bbb38824cfd72b104d35d3
O-Show <oshow999@gmail.com>
2015-11-22 17:24:52 +0900

    Add NHELP code representation struct
    "Step" is a NHELP code structure.
    "Step" has a "Routine",
    "Routine" explains what the "Step" do.

--- nhelpviz.go
+++ nhelpviz.go
@@ -32,5 +32,41 @@ func process() int {
 	}
 
 	fmt.Println(options)
+
+	nhelp := parseNHELP(options.InputFile)
+	fmt.Println(nhelp.routine)
+
 	return 0
+}
+
+type Routine int
+
+const (
+	BRAIN1 Routine = iota
+	BUILD1
+	CHANGE
+	DIVIDE
+	GATHER
+	GRAPH1
+	INDEX1
+	INPUT1
+	MATCH1
+	MATRIX
+	MERGE1
+	PRINT1
+	PRINT2
+	REPORT
+	SEARCH
+	SORT
+	SUMUP1
+	UPDATE
+)
+
+type Step struct {
+	routine Routine // kind of routine like BUILD1, INPUT1, etc.
+}
+
+func parseNHELP(inputFile string) Step {
+  // no implementation
+  return Step{routine: BUILD1}
 }
